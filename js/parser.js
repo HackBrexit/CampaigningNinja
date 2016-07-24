05d02af49cc7a291780befe87da0ebdcb3ad4e33
RhysWilliams12 <rhyscmn@live.co.uk>
2016-07-24 12:23:08 +0100

    front-end changes

--- js/parser.js
+++ js/parser.js
@@ -1,15 +1,31 @@
-var fs = require('fs');
-fs.readFile('London_postcodes.csv', function (err,logData){
-	if(err) throw err;
-	var text = logData.toString();
-	var lines = text.split('\n');
-	var header = lines[0];
-	
-	for(var i = 1; i < lines.length; i++){
-		var parts = lines[i];
 
-	}
+	var fs = require('fs');
+	fs.readFile('data/London_postcodes.csv', function (err,logData){
+		if(err) throw err;
+		var text = logData.toString();
+		var lines = text.split('\n');
+		var header = lines[0];
+		html = "<thead>";
+		for(var i = 0; i<header.length;i++){
+			html += "<th>"+header[i]+"</th>";
+		}
+	
+		for(var i = 1; i < lines.length; i++){
+			var parts = lines[i].split(',');
 		
-		var parts = line.split(',');
+			html += "</thead><tbody>";
 		
-});
+			for(var i = 1 ; i < lines.length; i++){
+				html += "<tr>";
+				var row = lines[i];
+				for(var j = 0; j < row.length; j++){
+					html += "<td>"+ row[j] + "</td>";
+				}
+				html+="</tr>";
+			}
+			html += "</tbody>";
+			$("#table-data").append(html);
+		}
+		
+	});
+
