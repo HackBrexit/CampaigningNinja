ffbc545d005c0618d52a45a4e48386767ef75214
RhysWilliams12 <rhyscmn@live.co.uk>
2016-08-25 14:45:43 +0100

    new layout

--- app.js
+++ app.js
@@ -2,21 +2,7 @@ var express = require('express');
 
 var app = express();
 var path = require('path');
-var request = require("request");
-var MongoClient = require('mongodb').MongoClient;
 
-
-var url = 'mongodb://localhost:27017/campaigning-ninja';
-MongoClient.connect(url,function(err,db){
-	if(!err){
-		console.log("Connected to db server");
-		insertDoc(db,function(){
-			db.close();
-		});
-	} else {
-		console.log("failed to connect to server");
-	}
-});
 
 
 app.use(express.static('public'));
